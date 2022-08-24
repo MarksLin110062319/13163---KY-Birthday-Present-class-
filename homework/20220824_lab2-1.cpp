04643e1b81567949aafa508e8e83d2aa94ade24e
MarksLin110062319 <92087185+MarksLin110062319@users.noreply.github.com>
2022-08-24 10:43:01 +0800

    Add files via upload

---
+++ homework/20220824_lab2-1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    string now_s = s;
+    int cnt = s.size();
+    while(cnt--){
+        string c = s.substr(0, 1);
+        s = s.substr(1, s.size()-1);
+        s = s + c;
+
+        if(s < now_s)
+            now_s = s;
+    }
+    cout << now_s << endl;
+}
